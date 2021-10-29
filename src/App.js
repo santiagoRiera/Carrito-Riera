2f5d93d32973f8e2a55218692918ce6551669d78
Santiago Riera <santilrier@gmail.com>
2021-10-29 14:31:10 -0300

    Creo navbar con MUI

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { ThemeProvider } from '@emotion/react';
+import { createTheme } from '@mui/material';
+import * as themes from './themes.js';
+import NavBar from './components/navbar/NavBar';
+
+const customTheme = createTheme(themes.theme1)
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={customTheme}>
+      <div className="App">
+        <NavBar/>
+      </div>
+    </ThemeProvider>
+    
+    
   );
 }
 
---
+++ src/themes.js
@@ -0,0 +1,12 @@
+export const theme1 = {  
+    palette:{
+      primary:{
+        main:'#fff9e7',
+        light:'#31c586',
+        dark:'#0d6648'
+      },
+      secondary:{
+        main: '#212121'
+      }
+    }
+}
